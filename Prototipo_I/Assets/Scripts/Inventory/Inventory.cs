c66f7f3246e1fe7067ce84ddc7a666f160f07210
eugenivasiliev <eugeni.vasiliev@enti.cat>
2025-10-12 18:54:39 +0200

    Added basic functionality

--- Prototipo_I/Assets/Scripts/Inventory/Inventory.cs
+++ Prototipo_I/Assets/Scripts/Inventory/Inventory.cs
@@ -2,9 +2,26 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-[Serializable]
-public class Inventory
+public class Inventory : MonoBehaviour 
 {
-    private List<Item> items;
+    private static Inventory instance;
+    public static Inventory Instance { get { return instance; } }
 
+    [SerializeField] private List<Item> items;
+
+    public void AddItem(Item item) => items.Add(item);
+
+    public void RemoveItem(Item item) => items.Remove(item);
+
+    public void Clear() => items.Clear();
+
+    public void Save()
+    {
+        FileManager.SaveFile(FileManager.InventoryFile, items);
+    }
+
+    public void Load()
+    {
+        FileManager.LoadFile(FileManager.InventoryFile, out items);
+    }
 }
--- Prototipo_I/Assets/Scripts/Inventory/Item.cs
+++ Prototipo_I/Assets/Scripts/Inventory/Item.cs
@@ -4,5 +4,8 @@ using UnityEngine;
 [Serializable]
 public class Item
 {
-    
+    public string Name; 
+    public string Description;
+
+    public virtual void OnUse() {}
 }
