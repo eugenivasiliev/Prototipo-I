413756133241dfc7355c51b74c34d1cf91c77ba6
eugenivasiliev <eugeni.vasiliev@enti.cat>
2025-10-14 23:20:02 +0200

    Added functional Hybridation according to a hybridation table

--- Prototipo_I/Assets/Scripts/Farm/PlotManager.cs
+++ Prototipo_I/Assets/Scripts/Farm/PlotManager.cs
@@ -6,6 +6,8 @@ public class PlotManager : MonoBehaviour
     
     public static PlotManager Instance;
     private List<Plot> plots = new List<Plot>();
+    [SerializeField] private HybridationManager hybridationManager;
+    public HybridationManager HybridationManager {  get { return hybridationManager; } }
 
     private void Awake()
     {
