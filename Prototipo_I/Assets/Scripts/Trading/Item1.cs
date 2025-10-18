e63217d6b0eb267a0fcc22b126269449080e04e4
eugenivasiliev <eugeni.vasiliev@enti.cat>
2025-10-18 13:23:08 +0200

    Added base for Trading UI

--- Prototipo_I/Assets/Scripts/Trading/Item1.cs
+++
@@ -1,9 +0,0 @@
-using Trading;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Item1", menuName = "Scriptable Objects/Item1")]
-public class Item1 : ScriptableObject, ITradeable
-{
-    [SerializeField] private int price = 100;
-    public int Price { get => price; set => price = value; }
-}
--- Prototipo_I/Assets/Scripts/Trading/TradingManager.cs
+++ Prototipo_I/Assets/Scripts/Trading/TradingManager.cs
@@ -2,8 +2,9 @@ using System;
 using System.Collections.Generic;
 using Trading;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class TradingManager : MonoBehaviour
+public class TradingManager : MonoBehaviour, IInteractable
 {
     private static TradingManager instance;
     public static TradingManager Instance {  get { return instance; } }
@@ -12,6 +13,12 @@ public class TradingManager : MonoBehaviour
 
     private ITradeable weeklyObjective;
     public ITradeable WeeklyObjective { get { return weeklyObjective; } }
+
+    [SerializeField] private GameObject tradingUI;
+
+    public List<IInteractable.KeyBinding> keyBindings => new List<IInteractable.KeyBinding> { 
+    new IInteractable.KeyBinding("trade", InputActionChange.ActionCanceled, ToggleTrading)
+    };
 
     private void Start()
     {
@@ -23,10 +30,33 @@ public class TradingManager : MonoBehaviour
         Destroy(gameObject);
     }
 
-    public bool TryBuy(ITradeable tradeable, int availableMoney)
+    public bool TryBuy(ITradeable tradeable)
     {
-        foreach (var item in stock)
-            if (item.Item1 == tradeable && availableMoney >= item.Item2) return true;
+        for (int i = 0; i < stock.Count; ++i)
+            if (stock[i].Item1 == tradeable && PlayerController.Instance.Money >= stock[i].Item1.Price)
+            {
+                PlayerController.Instance.Money -= stock[i].Item1.Price;
+                if (stock[i].Item2 > 1)
+                    stock[i] = Tuple.Create(stock[i].Item1, stock[i].Item2 - 1);
+                else
+                    stock.RemoveAt(i);
+                return true;
+            }
+        return false;
+    }
+
+    public bool TryBuy(ITradeable tradeable, int amount)
+    {
+        for (int i = 0; i < stock.Count; ++i)
+            if (stock[i].Item1 == tradeable && PlayerController.Instance.Money >= stock[i].Item1.Price * amount)
+            {
+                PlayerController.Instance.Money -= stock[i].Item1.Price * amount;
+                if (stock[i].Item2 > 1)
+                    stock[i] = Tuple.Create(stock[i].Item1, stock[i].Item2 - 1);
+                else
+                    stock.RemoveAt(i);
+                return true;
+            }
         return false;
     }
 
@@ -44,5 +74,15 @@ public class TradingManager : MonoBehaviour
     public ITradeable GetNewWeeklyObjective()
     {
         throw new NotImplementedException();
+    }
+
+    public void OnInteract()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void ToggleTrading(InputAction.CallbackContext ctx)
+    {
+        tradingUI.SetActive(!tradingUI.activeSelf);
     }
 }
