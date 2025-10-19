e80875890b584b851209750873bf4e49c79e0f74
eugenivasiliev <eugeni.vasiliev@enti.cat>
2025-10-19 22:50:34 +0200

    Modified indicator Input to InputSystem

--- Prototipo_I/Assets/Scripts/Inventory/Indicator.cs
+++ Prototipo_I/Assets/Scripts/Inventory/Indicator.cs
@@ -6,18 +6,6 @@ public class Indicator : MonoBehaviour
     private RectTransform rt;
     [SerializeField] private float scaledToItem = 1.3f;
 
-    void Update()
-    {
-        for(int i = 0; i < Inventory.Instance.InventorySpace; i++)
-        {
-            if (Input.GetKeyDown(KeyCode.Alpha1 + i))
-            {
-                CurrentIndex = i;
-                rt.position = Inventory.Instance.GetItemUIPosition(i);
-            }
-        }
-    }
-
     public void Initialize(Vector2 itemSize)
     {
         rt = GetComponent<RectTransform>();
@@ -25,5 +13,17 @@ public class Indicator : MonoBehaviour
         rt.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, itemSize.y * scaledToItem);
         rt.position = new Vector2(-Screen.width, -Screen.height);
         CurrentIndex = -1;
+
+        //Performed individually, since local variables aren't conserved as constant
+        PlayerController.Inputs.FindAction("Alpha1").canceled += ctx => { CurrentIndex = 0; MoveToCurItem(); };
+        PlayerController.Inputs.FindAction("Alpha2").canceled += ctx => { CurrentIndex = 1; MoveToCurItem(); };
+        PlayerController.Inputs.FindAction("Alpha3").canceled += ctx => { CurrentIndex = 2; MoveToCurItem(); };
+        PlayerController.Inputs.FindAction("Alpha4").canceled += ctx => { CurrentIndex = 3; MoveToCurItem(); };
+        PlayerController.Inputs.FindAction("Alpha5").canceled += ctx => { CurrentIndex = 4; MoveToCurItem(); };
+        PlayerController.Inputs.FindAction("Alpha6").canceled += ctx => { CurrentIndex = 5; MoveToCurItem(); };
+        PlayerController.Inputs.FindAction("Alpha7").canceled += ctx => { CurrentIndex = 6; MoveToCurItem(); };
+        PlayerController.Inputs.FindAction("Alpha8").canceled += ctx => { CurrentIndex = 7; MoveToCurItem(); };
     }
+
+    private void MoveToCurItem() => rt.position = Inventory.Instance.GetItemUIPosition(CurrentIndex);
 }
