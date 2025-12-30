770e73103619b29d2a2068c9027c73b73f17503a
eugenivasiliev <eugeni.vasiliev@enti.cat>
2025-12-30 10:31:23 +0100

    Added base for Tower placement

---
+++ Prototipo_I/Assets/Scripts/TowerDefense/Tower.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine.Events;
+
+public class Tower
+{
+    private int maxStage;
+
+    public int MaxStage => maxStage;
+
+    private int currentStage;
+
+    public int CurrentStage => currentStage;
+
+
+    public string Name { get; private set; }
+    public bool IsFullyUpgraded { get { return currentStage >= maxStage - 1; } }
+
+    public Action<int> OnStageChanged;
+
+    public Tower(TowerData data)
+    {
+        Name = data.Name;
+        maxStage = data.stages.Length;
+        currentStage = 0;
+    }
+}
---
+++ Prototipo_I/Assets/Scripts/TowerDefense/TowerData.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "TowerData", menuName = "Scriptable Objects/TowerData")]
+public class TowerData : ScriptableObject
+{
+    public string Name;
+
+    public GameObject[] stages;
+}
