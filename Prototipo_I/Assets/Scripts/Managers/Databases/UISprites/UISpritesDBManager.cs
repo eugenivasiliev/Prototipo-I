83d0ec697c1de8513a11965508c1ac3029a4a19e
eugenivasiliev <eugeni.vasiliev@enti.cat>
2026-02-10 10:40:07 +0100

    Blocked out basic copy of Durststein map from Thronefall

--- Prototipo_I/Assets/Scripts/Managers/Databases/UISprites/UISpritesDBManager.cs
+++ Prototipo_I/Assets/Scripts/Managers/Databases/UISprites/UISpritesDBManager.cs
@@ -5,7 +5,7 @@ public class UISpritesDBManager : Singleton<UISpritesDBManager>
     [SerializeField] private UISpritesDB dB;
     public UISpritesDB DB { get => dB; }
 
-    private void Start()
+    private void Awake()
     {
         Init();
         dB.Init();
