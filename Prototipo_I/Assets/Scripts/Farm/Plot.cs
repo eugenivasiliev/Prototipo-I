07db476e83aa572498d0d4ba64752c71031f872d
WalterScherk <walter.scherk@enti.cat>
2026-02-15 19:53:26 +0100

    Better dirt particles

--- Prototipo_I/Assets/Scripts/Farm/Plot.cs
+++ Prototipo_I/Assets/Scripts/Farm/Plot.cs
@@ -26,6 +26,7 @@ public class Plot : MonoBehaviour, IInteractable, IDamageable
 
     public GameObject plantingFeedback;
     public GameObject ripeFeedback;
+    private GameObject ripeParticles;
     public GameObject harvestFeedback;
 
     private void Awake()
@@ -67,7 +68,7 @@ public class Plot : MonoBehaviour, IInteractable, IDamageable
 
         Debug.Log($"Planta {plant.Name} plantada!");
 
-        Instantiate(plantingFeedback);        
+        Instantiate(plantingFeedback, transform.position, Quaternion.identity, transform);        
     }
 
     public void Fertilize()
@@ -133,8 +134,8 @@ public class Plot : MonoBehaviour, IInteractable, IDamageable
         statusText.text = $"{plant.Name}\n" +
                           $"Stage: {plant.CurrentStage} / 2\n" +
                           $"Time to next stage: {plant.TimeLeft:F1}s\n" +
-                          $"Watered: {(hasWater ? "Sí" : "No")}\n" +
-                          $"Fertilized: {(isFertilized ? "Sí" : "No")}";
+                          $"Watered: {(hasWater ? "Sï¿½" : "No")}\n" +
+                          $"Fertilized: {(isFertilized ? "Sï¿½" : "No")}";
     }
 
     private void OnPlantStageChanged(int currentStage)
@@ -182,6 +183,8 @@ public class Plot : MonoBehaviour, IInteractable, IDamageable
             Harvest();
             Debug.Log("Coshechada");
         }
+
+        Destroy(ripeParticles);
     }
 
     private void Action_Fertilize(InputAction.CallbackContext ctx)
@@ -196,6 +199,8 @@ public class Plot : MonoBehaviour, IInteractable, IDamageable
             return;
 
         OnPlantStageChanged(plant.CurrentStage -1);
+
+        ripeParticles = Instantiate(ripeFeedback, transform.position, Quaternion.identity, transform);
     }
     public void OnInteract() {}
 }
