225e23a208cad6585b35c8e29af8ff43713c4afc
WalterScherk <walter.scherk@enti.cat>
2026-02-23 13:20:59 +0100

    Show quantity of first ingredient

--- Prototipo_I/Assets/Scripts/UI/TowerMenu.cs
+++ Prototipo_I/Assets/Scripts/UI/TowerMenu.cs
@@ -56,8 +56,9 @@ public class TowerMenu : MonoBehaviour
 
         
         GameObject instance = Instantiate(towerUI, this.transform.GetChild(1));
-        //instance.GetComponent<Image>().sprite = UISpritesDBManager.Instance.DB["GasPlant"];        
-        instance.GetComponent<Image>().sprite = UISpritesDBManager.Instance.DB[td.ingredients[0].itemName]; 
+
+        instance.GetComponent<Image>().sprite = UISpritesDBManager.Instance.DB[td.ingredients[0].itemName];
+        instance.GetComponentInChildren<TMP_Text>().text = td.ingredients[0].amount.ToString();
     }
     
     public void EraseValidIngredients()
