9098cb49610110a0203162aaaf08ece06cf07dce
eugenivasiliev <eugeni.vasiliev@enti.cat>
2026-02-27 14:42:04 +0100

    Added Stun functionality in preparation for Octobear

--- Prototipo_I/Assets/Input/InputSystem_Actions.cs
+++ Prototipo_I/Assets/Input/InputSystem_Actions.cs
@@ -343,6 +343,15 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": false
+                },
+                {
+                    ""name"": ""debug"",
+                    ""type"": ""Button"",
+                    ""id"": ""bbc1dcdc-7cfc-4d7d-b042-ad3c8e18c7a2"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
                 }
             ],
             ""bindings"": [
@@ -937,6 +946,17 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""groups"": """",
                     ""action"": ""camera_zoom"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""18e384b7-c24b-4649-8207-ca9d1a4a0218"",
+                    ""path"": ""<Keyboard>/#(º)"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""debug"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
                 }
@@ -1552,6 +1572,7 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
         m_Player_refill_tower = m_Player.FindAction("refill_tower", throwIfNotFound: true);
         m_Player_objectives_toggle = m_Player.FindAction("objectives_toggle", throwIfNotFound: true);
         m_Player_camera_zoom = m_Player.FindAction("camera_zoom", throwIfNotFound: true);
+        m_Player_debug = m_Player.FindAction("debug", throwIfNotFound: true);
         // UI
         m_UI = asset.FindActionMap("UI", throwIfNotFound: true);
         m_UI_Navigate = m_UI.FindAction("Navigate", throwIfNotFound: true);
@@ -1673,6 +1694,7 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
     private readonly InputAction m_Player_refill_tower;
     private readonly InputAction m_Player_objectives_toggle;
     private readonly InputAction m_Player_camera_zoom;
+    private readonly InputAction m_Player_debug;
     /// <summary>
     /// Provides access to input actions defined in input action map "Player".
     /// </summary>
@@ -1797,6 +1819,10 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
         /// </summary>
         public InputAction @camera_zoom => m_Wrapper.m_Player_camera_zoom;
         /// <summary>
+        /// Provides access to the underlying input action "Player/debug".
+        /// </summary>
+        public InputAction @debug => m_Wrapper.m_Player_debug;
+        /// <summary>
         /// Provides access to the underlying input action map instance.
         /// </summary>
         public InputActionMap Get() { return m_Wrapper.m_Player; }
@@ -1906,6 +1932,9 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
             @camera_zoom.started += instance.OnCamera_zoom;
             @camera_zoom.performed += instance.OnCamera_zoom;
             @camera_zoom.canceled += instance.OnCamera_zoom;
+            @debug.started += instance.OnDebug;
+            @debug.performed += instance.OnDebug;
+            @debug.canceled += instance.OnDebug;
         }
 
         /// <summary>
@@ -2001,6 +2030,9 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
             @camera_zoom.started -= instance.OnCamera_zoom;
             @camera_zoom.performed -= instance.OnCamera_zoom;
             @camera_zoom.canceled -= instance.OnCamera_zoom;
+            @debug.started -= instance.OnDebug;
+            @debug.performed -= instance.OnDebug;
+            @debug.canceled -= instance.OnDebug;
         }
 
         /// <summary>
@@ -2497,6 +2529,13 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
         /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
         /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
         void OnCamera_zoom(InputAction.CallbackContext context);
+        /// <summary>
+        /// Method invoked when associated input action "debug" is either <see cref="UnityEngine.InputSystem.InputAction.started" />, <see cref="UnityEngine.InputSystem.InputAction.performed" /> or <see cref="UnityEngine.InputSystem.InputAction.canceled" />.
+        /// </summary>
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.started" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
+        void OnDebug(InputAction.CallbackContext context);
     }
     /// <summary>
     /// Interface to implement callback methods for all input action callbacks associated with input actions defined by "UI" which allows adding and removing callbacks.
