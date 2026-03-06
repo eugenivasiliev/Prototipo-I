2910d72a403dee4ad01ba3a365527033bf3251d6
WalterScherk <walter.scherk@enti.cat>
2026-03-06 20:33:53 +0100

    Windmill base, and vines holeigan minor correction

--- Prototipo_I/Assets/Scripts/TowerDefense/Vines.cs
+++ Prototipo_I/Assets/Scripts/TowerDefense/Vines.cs
@@ -5,7 +5,6 @@ public class Vines : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<EnemyAI>() != null) {
-            Debug.Log("Slowing down");
             other.GetComponent<EnemyAI>().Slow();
         }
     }
