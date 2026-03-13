95212230febdedd7264e7fc4c5380d3d17e6c594
eugenivasiliev <eugeni.vasiliev@enti.cat>
2026-03-13 10:47:40 +0100

    Added Win and Loss conditions with full flow

--- Prototipo_I/Assets/Scripts/Managers/TitleScreen.cs
+++ Prototipo_I/Assets/Scripts/Managers/TitleScreen.cs
@@ -9,6 +9,8 @@ namespace UI
         void Start()
         {
             AudioManager.Instance.PlayMusic("TitleScene");
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
         }
 
         // Update is called once per frame
