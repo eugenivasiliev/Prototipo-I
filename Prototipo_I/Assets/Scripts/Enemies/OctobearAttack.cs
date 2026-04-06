864f4b62a6e92933685de6fae01dc8b91732e7d7
WalterScherk <walter.scherk@enti.cat>
2026-04-06 15:42:19 +0200

    Octobear continuous attack

--- Prototipo_I/Assets/Scripts/Enemies/OctobearAttack.cs
+++ Prototipo_I/Assets/Scripts/Enemies/OctobearAttack.cs
@@ -1,18 +1,39 @@
 using Player;
 using UnityEngine;
+using System.Collections;
 
 namespace Enemies { 
 
     public class OctobearAttack : MonoBehaviour
     {
         private float stunTime = 1.0f;
+        private float attackCooldown = 1.0f;
+        private Collider col;
 
         private void OnTriggerEnter(Collider other)
         {
             if (other.tag == "Player")
             {
-                other.GetComponent<PlayerController>().Stun(stunTime);
+                col = other;
+                StartCoroutine(StunStart());
+            }
+        }
 
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.tag == "Player")
+                col = null;
+        }
+
+        private IEnumerator StunStart() {
+
+            col.GetComponent<PlayerController>().Stun(stunTime);
+
+            yield return new WaitForSeconds(attackCooldown);
+
+            if (col != null)
+            {
+                StartCoroutine(StunStart());
             }
         }
     }
