6367f7dffbbfc31942850b897322a7adbd09c514
WalterScherk <walter.scherk@enti.cat>
2026-04-25 18:07:01 +0200

    Turret and planting tutorial

--- Prototipo_I/Assets/Scripts/AICompanion/DialogueTrigger.cs
+++ Prototipo_I/Assets/Scripts/AICompanion/DialogueTrigger.cs
@@ -6,10 +6,10 @@ namespace AICompanion
 {
     public class DialogueTrigger : MonoBehaviour
     {
-        private float time = 5.0f;
+        
         private PlayerController playerController;
         [SerializeField] private string dialogueId;
-        [SerializeField] private bool shouldStop;
+        
         [SerializeField] private GameObject wall;
 
         private void OnTriggerEnter(Collider other)
@@ -17,24 +17,13 @@ namespace AICompanion
             if (other.tag == "Player") { 
                 AICompanionDialogueUI.Instance.DisplayTextById(dialogueId);
 
-                if (shouldStop) 
-                    StartCoroutine(StopForATime(other));
+                ActivateNextTutorial();
             }
 
         }
 
 
 
-        IEnumerator StopForATime(Collider c)
-        {
-            c.GetComponent<PlayerController>().MovementLocked = true;
-
-            yield return new WaitForSeconds(time);
-            
-            c.GetComponent<PlayerController>().MovementLocked = false;
-
-            ActivateNextTutorial();
-        }
 
         void ActivateNextTutorial() {
             Destroy(gameObject);
