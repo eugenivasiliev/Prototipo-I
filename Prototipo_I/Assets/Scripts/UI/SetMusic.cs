a100da82453e8b16baa60509744f32499f8b0870
eugenivasiliev <eugeni.vasiliev@enti.cat>
2026-04-29 02:54:41 +0200

    Implemented new music and SFX

--- Prototipo_I/Assets/Scripts/UI/SetMusic.cs
+++ Prototipo_I/Assets/Scripts/UI/SetMusic.cs
@@ -1,13 +1,27 @@
+using System.Collections.Generic;
 using Audio;
 using UnityEngine;
+using Utils;
 
 namespace UI
 {
     public class SetMusic : MonoBehaviour
     {
+        [SerializeField] private List<string> phaseMusics = new List<string> { "GameSceneDay", "GameSceneNight" };
+        private int curPhaseMusic = 0;
+
         void Start()
         {
-            AudioManager.Instance.PlayMusic("GameSceneDay");
+            AudioManager.Instance.PlayMusic(phaseMusics[curPhaseMusic]);
+            DayNightCycle.Instance.SubscribeTimedEvent(ChangePhaseMusic, 1);
+        }
+
+        private void ChangePhaseMusic(float t)
+        {
+            curPhaseMusic++;
+            curPhaseMusic %= phaseMusics.Count;
+            AudioManager.Instance.PlayMusic(phaseMusics[curPhaseMusic]);
+            DayNightCycle.Instance.SubscribeTimedEvent(ChangePhaseMusic, 1);
         }
 
     }
