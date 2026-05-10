3ec4ebe238b16126fa382ae900ff6d7d35fcda0e
eugenivasiliev <eugeni.vasiliev@enti.cat>
2026-05-10 15:14:51 +0200

    Implemented missing sounds in game

--- Prototipo_I/Assets/Scripts/UI/SetMusic.cs
+++ Prototipo_I/Assets/Scripts/UI/SetMusic.cs
@@ -7,11 +7,13 @@ namespace UI
 {
     public class SetMusic : MonoBehaviour
     {
+        [SerializeField] private string ambience;
         [SerializeField] private List<string> phaseMusics = new List<string> { "GameSceneDay", "GameSceneNight" };
         private int curPhaseMusic = 0;
 
         void Start()
         {
+            if(ambience != "") AudioManager.Instance.PlayAmbientEvent(ambience);
             AudioManager.Instance.PlayMusicEvent(phaseMusics[curPhaseMusic]);
             DayNightCycle.Instance.SubscribeTimedEvent(ChangePhaseMusic, 1);
         }
