eb641649b7b70d3934be28fa22affdd8027eba7f
Walter Scherk Fontanals <walter.scherk@enti.cat>
2026-05-27 12:30:39 +0200

    dissolving base

---
+++ Prototipo_I/Assets/Scripts/Tutorial/FenceDissolver.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class FenceDissolver : MonoBehaviour
+{
+    [SerializeField] GameObject[] things;
+    
+    [SerializeField] GameObject[] fences;
+
+    [SerializeField] GameObject waitDialogue;
+    [SerializeField] GameObject console;
+    [SerializeField] GameObject dullConsole;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < things.Length; i++)
+        {
+            if (things[i] != null) return;
+        }
+
+
+
+        for (int i = 0; i < fences.Length; i++)
+        {
+            StartCoroutine(fences[i].GetComponent<StickHolder>().Dissolve());
+
+        }
+
+
+        waitDialogue.SetActive(true);
+        if (console != null)
+            console.SetActive(true);
+        if (dullConsole != null)
+            dullConsole.SetActive(false);
+
+
+        Destroy(gameObject);
+    }
+}
